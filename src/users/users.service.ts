ecf5d5a4955d8c689aef09674ec93d8859188904
L1SARD7 <arthemklimchuk7@gmail.com>
2026-04-20 04:17:57 +0300

    feat: add user schema and database service

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(
+    email: string,
+    passwordHash: string,
+    name: string,
+  ): Promise<UserDocument> {
+    const createdUser = new this.userModel({ email, passwordHash, name });
+    return createdUser.save();
+  }
+
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
