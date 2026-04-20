35eecfb71b0158b44af50a55fb492e729b8e9176
L1SARD7 <arthemklimchuk7@gmail.com>
2026-04-20 11:14:15 +0300

    refactor: remove unused analytics module from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { CommentsModule } from './comments/comments.module';
 import { AuthModule } from './auth/auth.module';
-import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
   imports: [
@@ -25,7 +24,6 @@ import { AnalyticsModule } from './analytics/analytics.module';
     TasksModule,
     CommentsModule,
     AuthModule,
-    AnalyticsModule,
   ],
 })
 export class AppModule {}
