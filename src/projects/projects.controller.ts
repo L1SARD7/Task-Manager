7d3d6f8f7cc2be56d108432b15e2973731e793e9
L1SARD7 <arthemklimchuk7@gmail.com>
2026-04-20 11:08:13 +0300

    feat: add CurrentUser to projects controller

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -8,40 +8,49 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('projects')
 export class ProjectsController {
-  private readonly fakeUserId = '60d5ecb8b392d700153ee123';
-
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectsService.create(createProjectDto, this.fakeUserId);
+  create(
+    @Body() createProjectDto: CreateProjectDto,
+    @CurrentUser('userId') userId: string, // Дістаємо ID прямо з токена!
+  ) {
+    return this.projectsService.create(createProjectDto, userId);
   }
 
   @Get()
-  findAll() {
-    return this.projectsService.findAllUserProjects(this.fakeUserId);
+  findAll(@CurrentUser('userId') userId: string) {
+    return this.projectsService.findAllUserProjects(userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectsService.findOne(id, this.fakeUserId);
+  findOne(@Param('id') id: string, @CurrentUser('userId') userId: string) {
+    return this.projectsService.findOne(id, userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectsService.update(id, updateProjectDto, this.fakeUserId);
+  update(
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+    @CurrentUser('userId') userId: string,
+  ) {
+    return this.projectsService.update(id, updateProjectDto, userId);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
-    return this.projectsService.remove(id, this.fakeUserId);
+  remove(@Param('id') id: string, @CurrentUser('userId') userId: string) {
+    return this.projectsService.remove(id, userId);
   }
 }
